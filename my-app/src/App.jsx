80649a1427e213aaa5cc23d085222afae686b712
Shivam Kumar <kshivam0508@gmail.com>
2025-08-03 19:31:02 +0530

    03-08-2025 add MapFunction

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,12 +2,13 @@
 import React from "react";
 import { useState } from "react";
 import SelectBox from "./Component1/SelectBox";
+import MapFunction from "./Component1/MapFunction";
 
 function App() {
   return (
     <> 
     <h1>React Vite</h1>
-    <SelectBox />
+    <MapFunction />
     </>
   );
 }
---
+++ my-app/src/Component1/MapFunction.jsx
@@ -0,0 +1,56 @@
+function MapFunction() {
+ const userName = ["Shivamiva", "hhgf", "hkglk", "u;oug", "uuh;jgh;"];
+    const userData = [
+        {
+            name: "Anil",
+            age: "20",
+            email: "anil@gmail.com",
+            phone: 1234567890,
+            id: 1
+        },
+        {
+            name: "hhgf",
+            age: "20",
+            email: "hhghd@gmail.com",
+            phone: 1234567890,
+            id: 2
+        },
+        {
+            name: "hkglk",
+            age: "11",
+            email: "anil@gmail.com",
+            phone: 1234567890,
+            id: 3
+        }
+    ];
+
+  return (
+    <div>
+      <h1>Map Function</h1>
+<tabel border="2">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Email</th>
+            <th>Phone</th>
+        </tr>
+    </thead>
+    <tbody>
+        {userData.map((user)=>(
+            <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td> {user.age}</td>
+                <td>{user.email}</td>
+                <td>{user.phone}</td>
+            </tr>
+))}
+    </tbody>
+      
+</tabel>
+    </div>
+  );
+}
+export default MapFunction;
