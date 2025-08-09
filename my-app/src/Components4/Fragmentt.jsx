700c08f6494f2dcfd5a594d5223e4f8cbbd33ef5
Shivam Kumar <kshivam0508@gmail.com>
2025-08-09 11:54:55 +0530

    Fragment also app

--- my-app/src/Components4/Fragmentt.jsx
+++ my-app/src/Components4/Fragmentt.jsx
@@ -7,3 +7,30 @@ function Fragmentt(){
     )
 }
 export default Fragmentt;
+/* 
+function App(){
+    return (
+        <>
+        <div>
+            <Data />
+            <Data />
+  <Data />
+            <Data />  <Data />
+            <Data />  <Data />
+            <Data />  <Data />
+            <Data />
+        </div>
+        </>
+    )
+}
+export default App;
+function Data(){
+    return(
+        <di>
+            <span>
+                Hello Fragment ""
+            </span>
+            <hr />
+        </di>
+    )
+}*/
