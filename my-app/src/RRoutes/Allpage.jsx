22a3d29f0d9b794186305fdb6ab18e05398ca34d
Shivam Kumar <kshivam0508@gmail.com>
2025-08-11 13:29:00 +0530

    RRoutes

--- my-app/src/RRoutes/Allpage.jsx
+++ my-app/src/RRoutes/Allpage.jsx
@@ -1,27 +1,24 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import Home from './RRoutes/Home';
-import Login from "./RRoutes/Login";
-import Index from "./RRoutes/Index";
-import Collage from "./API/Collage";
+import { Link, Route, Routes } from "react-router-dom";
+import Home from './Home';
+import Login from "./Login";
+import Index from "./Index";
 
-function App() {
+function AllPage() {
     return (
-        <BrowserRouter>
+        <>
             <nav>
                 <Link to="/"><h1>Home</h1></Link>
                 <Link to="/index"><h1>About</h1></Link>
                 <Link to="/login"><h1>Login</h1></Link>
-                <Link to="/collage"><h1>Collage</h1></Link>
             </nav>
 
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/index" element={<Index />} />
                 <Route path="/login" element={<Login />} />
-                <Route path="/collage" element={<Collage />} />
             </Routes>
-        </BrowserRouter>
+        </>
     );
 }
 
-export default App;
+export default AllPage;
---
+++ my-app/src/RRoutes/Index.jsx
@@ -0,0 +1,8 @@
+function Index() {
+    return (
+        <div>
+            <h1>About / Index</h1>
+        </div>
+    );
+}
+export default Index;
