146aae67477e1ec294b29c8ab080ebe8fcc27a8d
Shivam Kumar <kshivam0508@gmail.com>
2025-08-14 01:35:59 +0530

    BEE/SERVER/S1.js

--- my-app/BEE/server.js
+++ my-app/BEE/server.js
@@ -2,8 +2,23 @@ const express = require('express');
 const app = express();
 const port = 3030;
 
+// Middleware to parse JSON bodies
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');  // Note: 'res' not 'resp'
+    res.send('Hello World!');
+});
+
+app.post('/item', (req, res) => {  // Fixed this line
+    res.send('Hello World! post');
+});
+
+app.put('/item/:id', (req, res) => {  // Fixed this line
+    res.send(`Hello World! put - ID: ${req.params.id}`);
+});
+
+app.delete('/item', (req, res) => {  // Fixed this line
+    res.send('Hello World! delete');
 });
 
 app.listen(port, () => {
